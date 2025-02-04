d818cc174d3e0d085008d2155b00850f3068983d
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-02-04 23:50:35 +0530

    c

---
+++ 01_basics/01_practice.js
@@ -0,0 +1,18 @@
+let x = 3;
+console.log(x);
+
+let y = x++;
+console.log(y);
+
+let z = y++
+console.log(z);
+console.log(x,y,z);
+
+//onsole.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+//console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
