3c5a2e87def235a0fda64a6baf7e4a7e5dba3172
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-02-04 19:37:34 +0530

    conversionOfDataType

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,31 @@
+//let score = 44                 // o/p -> 44
+//let score = "44abc"            // o/p -> NaN
+//let score = null               // o/p -> 0
+//let score = undefined          // o/p -> NaN
+//let score = false              // o/p -> 0
+//let score = true               // o/p -> 1
+//let score = "Prashant"         // o/p -> NaN
+
+
+//const {score} = req.body 
+
+// console.log(typeof score) // string;
+// console.log(typeof(score));
+
+// let scoreNumber = Number(score)    //conversion
+// console.log(typeof scoreNumber)
+// console.log(scoreNumber);
+
+//let isloggedin = 1           // o/p -> true
+//let isloggedin = ""           // o/p -> false
+//let isloggedin = "Prshant"       // o/p -> true
+
+// let isloggedinBoolean = Boolean(isloggedin)    //conversion
+// console.log(typeof isloggedinBoolean);
+// console.log(isloggedinBoolean);
+let someNumber = 11
+
+let someString = String(someNumber)    //conversion
+console.log(typeof someString);
+console.log(someString);
+
