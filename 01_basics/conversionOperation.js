7dabaa02e367686128ea9e5acb5a9824011974bd
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-02-04 21:33:17 +0530

    operators

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -23,9 +23,45 @@
 // let isloggedinBoolean = Boolean(isloggedin)    //conversion
 // console.log(typeof isloggedinBoolean);
 // console.log(isloggedinBoolean);
-let someNumber = 11
+// let someNumber = 11
 
-let someString = String(someNumber)    //conversion
-console.log(typeof someString);
-console.log(someString);
+// let someString = String(someNumber)    //conversion
+// console.log(typeof someString);
+// console.log(someString);
+
+//***************************Operations**********************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2**3);
+// console.log(2%3);
+// let str1 = "Prashant"
+// let str2 = " Vishwakarma"
+// let str3 = (str1+str2)
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);           // agar string first me hai toh sabhi ko string ki tarah treat kiya jayega, aur agar string last me hai toh pahle operation karega fir string me convert karega
+// console.log(1+2+"2");          // 3+2 = 32 ()
+
+// console.log((3+4) * 5%3);      //use ,more parenthesis to avoid confusion
+
+// console.log(true);     // o/p -> true    (aise code likhana bahut kharab baat hai, avoid it)
+// console.log(+true);    // o/p -> 1
+// console.log(+"");      // o/p -> 0
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+let gameCounter = 100
+++gameCounter
+
+console.log(gameCounter);
+
+
 
