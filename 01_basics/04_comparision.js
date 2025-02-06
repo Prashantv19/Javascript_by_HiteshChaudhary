d9bcdefac4eb971a34cd270737ee87ca79d69252
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-02-06 23:38:43 +0530

    commit

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2==1);
+// console.log(2>=3);
+// console.log(2<=1);
+// console.log(2>4);
+// console.log(2<3);
+
+console.log("2" > 1);       // different data type conversion
+console.log("02" > 1);
+
+console.log(null>0);        // not recommended to write code like this
+console.log(null==0);        //
+console.log(null>=0);
+
+console.log(undefined >0);   // avoid karo aise code likhana
+console.log(undefined==0);
+console.log(undefined<0);
+
+console.log("2" === 2);  //strict check
+
+
