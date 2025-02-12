8df5f72c9c13e74d8353557e7687aea21eaab5f8
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-02-12 22:21:27 +0530

    stack and heap

--- 01_basics/05_dataType_Summary.js
+++ 01_basics/05_dataType_Summary.js
@@ -17,7 +17,7 @@ let userName;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);  //false
+//console.log(id === anotherId);  //false
 
 //const bigNumber = 1234567890123456789012345678901234567890n  //n is used to define BigInt
 
@@ -37,12 +37,37 @@ const Function = function(){
     console.log('Hello World');
 }
 
-console.log(typeof bigNumber); // BigInt
-console.log(typeof outSideTemp); // object
-console.log(typeof userName); // undefined
-console.log(typeof score);
-console.log(typeof Function);
+// console.log(typeof bigNumber); // BigInt
+// console.log(typeof outSideTemp); // object
+// console.log(typeof userName); // undefined
+// console.log(typeof score);
+// console.log(typeof Function);
 
 
 
+//-------------------------------------------------------------------------
+// thee are two types of memory
+// Stack(Primittive) -> ye jab use hoti hai tab isme ek copy milta hai 
+// Heap(Non-Primittive) -> isme original value ka referance milta hai
+// Stack memory is limited and Heap memory is unlimited
+let myYouTubename = "PrashantVishwakarma"
+let AnotherName = myYouTubename
 
+AnotherName = "thedeepocean"  
+// ye change sirf AnotherName me hoga myYouTubename me nahi hoga because ye ek copy bana deta hai aur changes usi me hote hai.
+
+console.log(AnotherName);
+console.log(myYouTubename);
+
+let user1 = {
+    email : "user@google.com",
+    upi: "user@okhdfc"
+}
+let user2 = user1
+user2.email = "prashant@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
+//jab heap ke ander koi bhi value change hoti hai to uska referance change hota hai to usme sabhi value change ho jati hai.
+//jo bhi changes hote hai wo original value me hote hai.
