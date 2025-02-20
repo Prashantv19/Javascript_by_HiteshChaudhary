62d5b085dc8d3a7c9d3d41a3fe7a06f16da40c3a
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-02-20 21:33:50 +0530

    Practice

--- 01_basics/01_variablesPractice.js
+++ 01_basics/01_variablesPractice.js
@@ -1,12 +1,12 @@
 let x = 3;
-console.log(x);
+// console.log(x);
 
 let y = x++;
-console.log(y);
+// console.lo
 
 let z = y++
-console.log(z);
-console.log(x,y,z);
+// console.log(z);
+// console.log(x,y,z);
 
 //onsole.log(`x:${x}, y:${y}`);
 // Expected output: "x:4, y:3"
@@ -16,3 +16,27 @@ const b = ++a;
 
 //console.log(`a:${a}, b:${b}`);
 // Expected output: "a:4, b:4"
+
+//-----------------------------------------------------------
+let A = "Prashant"
+console.log(A+5);
+
+
+console.log(typeof (A+5));
+
+const obj = {
+    name: "Prashant",
+    myage: 25
+}
+//console.log(obj);
+
+// obj ={
+//     Height: 5.8         // This will throw an error
+// }
+const obj1 = {
+    Name: "name",
+    age: 24,
+    width: 4,
+    height: 5
+}
+console.log(obj1)
