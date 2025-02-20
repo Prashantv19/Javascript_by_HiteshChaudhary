2e287455794335bbac385d2a2ca5dac65350542e
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-02-20 22:03:13 +0530

    new start

---
+++ 01_basics/video_55/script.js
@@ -0,0 +1,18 @@
+console.log("Hey this is tutorial 55");
+let a = 5;
+let b = 6;
+let c = "Prashant";
+
+
+console.log(a+b+5, " ", c);
+console.log(typeof a,typeof b, typeof c);
+// const a1 = 56;
+// a1 = a1 +2;    //this is not Allowed to add in constant variable
+{
+    let a1 = 45;
+    a1 = a1 + 5;
+    console.log(a1);
+    console.log("This is block scope");
+}
+console.log(a);
+
