001e9ad184298c1c4c82dc3ce4613323897e0481
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-02-24 23:12:03 +0530

    no mood

--- 01_basics/video_55/script.js
+++ 01_basics/video_55/script.js
@@ -1,21 +1,37 @@
-alert("hello, this is me")
+// alert("hello, this is me")
 
 
-console.log("Hey this is tutorial 55");
-let a = 5;
-let b = 6;
-let c = "Prashant";
+// console.log("Hey this is tutorial 55");
+// let a = 5;
+// let b = 6;
+// let c = "Prashant";
 
 
-console.log(a+b+5, " ", c);
-console.log(typeof a,typeof b, typeof c);
-// const a1 = 56;
-// a1 = a1 +2;    //this is not Allowed to add in constant variable
-{
-    let a1 = 45;
-    a1 = a1 + 5;
-    console.log(a1);
-    console.log("This is block scope");
+// console.log(a+b+5, " ", c);
+// console.log(typeof a,typeof b, typeof c);
+// // const a1 = 56;
+// // a1 = a1 +2;    //this is not Allowed to add in constant variable
+// {
+//     let a1 = 45;
+//     a1 = a1 + 5;
+//     console.log(a1);
+//     console.log("This is block scope");
+// }
+// console.log(a);
+
+let x = "Prashant"
+let y = 21
+let z = 3.1416
+const p = true
+let q = null
+let r = undefined
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x ,typeof y, typeof z, typeof p, typeof q, typeof r);
+let o = {
+    "name" : "Prahant",
+    "job": "SDE",
+    "age": 21
 }
-console.log(a);
+console.log(o);
 
