ca40b793d9e431c39a7151034e52e85af1c21118
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-02-28 00:20:30 +0530

    update

--- 01_basics/video_56/index.js
+++ 01_basics/video_56/index.js
@@ -1,22 +1,25 @@
-console.log("Hello im conditional tutorial");
+// console.log("Hello im conditional tutorial");
 
-let age = 17
-let grace = 2
+let age = 0
+let grace = 0
 age +=grace
-console.log(age);
+// console.log(age);
 
-console.log(age + grace);
-console.log(age - grace);
-console.log(age * grace);
-console.log(age / grace);
-console.log(age ** grace);
-console.log(age % grace);
-console.log(age == grace);
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+// console.log(age == grace);
 
 if((age+grace) >18){
     console.log("You are eligible to vote");
 }
-else{
+else if(age ==0){
+    console.log("You are not born yet");
+}else{
     console.log("You are not eligible to vote");
+    
 }
     
