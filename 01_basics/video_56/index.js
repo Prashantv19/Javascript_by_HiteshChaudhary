431e122af0e334f19c0d2ae59770c01dfb33bf85
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-02-27 23:57:43 +0530

    breakup code

---
+++ 01_basics/video_56/index.js
@@ -0,0 +1,22 @@
+console.log("Hello im conditional tutorial");
+
+let age = 17
+let grace = 2
+age +=grace
+console.log(age);
+
+console.log(age + grace);
+console.log(age - grace);
+console.log(age * grace);
+console.log(age / grace);
+console.log(age ** grace);
+console.log(age % grace);
+console.log(age == grace);
+
+if((age+grace) >18){
+    console.log("You are eligible to vote");
+}
+else{
+    console.log("You are not eligible to vote");
+}
+    
