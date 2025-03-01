df6bd3e1ac7ef977c0d8803aca4473f5940f914b
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-03-01 23:33:14 +0530

    practice

---
+++ 01_basics/01Practice/conditionalsAndOperators.js
@@ -0,0 +1,25 @@
+let a = "Prashant"
+let b = 22
+// console.log(40+60);
+// console.log(40-60);
+// console.log(40/60);
+// console.log(40*60);
+// console.log(40**60);
+// console.log(60%11);
+// b++
+// console.log(b);
+// b--
+// console.log(b);
+let x = 10
+let y = 20
+console.log(x=y);
+console.log(x);
+
+console.log(x+=y);
+console.log(x-=y);
+console.log(x*=y);
+console.log(x/=y);
+console.log(x%=y);
+console.log(x**=y);
+
+
