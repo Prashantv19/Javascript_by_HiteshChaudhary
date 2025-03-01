f5b33b9816e164c69cede8999f647ca1ba1ae7ec
Prashant Vishwakarma <prashantv1902@gmail.com>
2025-03-01 23:52:26 +0530

    code

--- 01_basics/01Practice/conditionalsAndOperators.js
+++ 01_basics/01Practice/conditionalsAndOperators.js
@@ -11,15 +11,54 @@ let b = 22
 // b--
 // console.log(b);
 let x = 10
-let y = 20
-console.log(x=y);
-console.log(x);
+let y = 10
+// // console.log(x=y);
+// console.log(x);
 
-console.log(x+=y);
-console.log(x-=y);
-console.log(x*=y);
-console.log(x/=y);
-console.log(x%=y);
-console.log(x**=y);
+// console.log(x+=y);
+// console.log(x-=y);
+// console.log(x*=y);
+// console.log(x/=y);
+// console.log(x%=y);
+// console.log(x**=y);
+
+// console.log(x==y);
+// console.log(x!=y);
+// console.log(x===y);
+// console.log(x!==y);
+// console.log(x>y);
+// console.log(x<y);
+// console.log(x>=y);
+// console.log(x<=y);
+// // console.log(x?y);
+
+// console.log((x>5)&&(y<10));
+// console.log((x>5)||(y<10));
+// // console.log(x||y);
+// console.log(!y);
 
 
+//-----------------Conditional Statements-----------------
+if(x>y){
+    console.log("x is greater than y");
+}
+if(x<y){
+    console.log("y is greater than x");
+}
+
+
+if(x>y){
+    console.log("x is greater than y");
+}else{
+    console.log("y is greater than x");
+}
+
+
+
+if(x<y){
+    console.log("x is less than y");
+}else if(x>y){
+    console.log("x is greater than y");
+}else{
+    console.log("x is equal to y");
+}
